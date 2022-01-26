910fae19ca82b85e4e30858d7d7734b1601eb22c
Gabriel Pires <gabpiresdev@gmail.com>
2022-01-26 18:28:18 -0300

    Creating SignIn screen and styles

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Image, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { ButtonIcon } from '../../components/ButtonIcon';
+
+import { styles } from './style';
+
+import IllustrationImg from '../../assets/illustration.png';
+
+export const SignIn = () => {
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor={'transparent'} style={'auto'} translucent />
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode={'stretch'}
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {'\n'}suas jogatinas {'\n'}facilmente
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {'\n'}favoritos com seus amigos
+        </Text>
+
+        <ButtonIcon />
+      </View>
+    </View>
+  );
+};
